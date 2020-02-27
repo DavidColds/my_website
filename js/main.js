bb65629d3fdc40bf4d9be44097c3c13e8b888f34
David Drughi <drughidavid6@gmail.com>
2020-02-27 19:19:50 +0100

    clean code and icons on footer

--- js/main.js
+++ js/main.js
@@ -3,55 +3,75 @@ function validateForm() {
     var e = document.getElementById('email').value;
     var s = document.getElementById('subject').value;
     var m = document.getElementById('message').value;
-    var onlyLetters =/^[a-zA-Z\s]*$/; 
+    var onlyLetters = /^[a-zA-Z\s]*$/;
     var onlyEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    
-    
-    if(n == "" || n == null){
+
+
+    if (n == "" || n == null) {
         document.getElementById('nameLabel').innerHTML = ('Please enter your name');
         document.getElementById('name').style.borderColor = "red";
         return false;
     }
-       
-  
+
+
     if (!n.match(onlyLetters)) {
         document.getElementById('nameLabel').innerHTML = ('Please enter only letters');
         document.getElementById('name').style.borderColor = "red";
         return false;
     }
-  
-    if(e == "" || e == null ){
-          document.getElementById('emailLabel').innerHTML = ('Please enter your email');
-          document.getElementById('email').style.borderColor = "red";
-          return false;
-      }
-  
+
+    if (e == "" || e == null) {
+        document.getElementById('emailLabel').innerHTML = ('Please enter your email');
+        document.getElementById('email').style.borderColor = "red";
+        return false;
+    }
+
     if (!e.match(onlyEmail)) {
         document.getElementById('emailLabel').innerHTML = ('Please enter a valid email address');
         document.getElementById('email').style.borderColor = "red";
         return false;
     }
-  
-    if(s == "" || s == null ){
-          document.getElementById('subjectLabel').innerHTML = ('Please enter your subject');
-          document.getElementById('subject').style.borderColor = "red";
-          return false;
-      }
-  
+
+    if (s == "" || s == null) {
+        document.getElementById('subjectLabel').innerHTML = ('Please enter your subject');
+        document.getElementById('subject').style.borderColor = "red";
+        return false;
+    }
+
     if (!s.match(onlyLetters)) {
         document.getElementById('subjectLabel').innerHTML = ('Please enter only letters');
         document.getElementById('subject').style.borderColor = "red";
         return false;
     }
-  
-    if(m == "" || m == null){
+
+    if (m == "" || m == null) {
         document.getElementById('messageLabel').innerHTML = ('Please enter your message');
         document.getElementById('message').style.borderColor = "red";
         return false;
+    } else {
+        return true;
     }
-  
-    else{
-          return true;
-      }
-      
+
 }
+
+
+
+
+var dummyContent = $('.dummy-content').children(),
+    i;
+
+
+$('#add-content').click(function (e) {
+    e.preventDefault();
+
+    if ($(dummyContent[0]).is(":visible")) {
+        for (i = 0; i < dummyContent.length; i++) {
+            $(dummyContent[i]).fadeOut(600);
+        }
+    } else {
+        for (i = 0; i < dummyContent.length; i++) {
+            $(dummyContent[i]).delay(600 * i).fadeIn(600);
+        }
+    }
+
+});
