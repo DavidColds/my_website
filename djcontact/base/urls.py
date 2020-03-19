f725499c9fb6419a0484d418440c6c3b3b80691d
David Drughi <drughidavid6@gmail.com>
2020-03-19 07:53:39 +0100

    email on the home page working

--- djcontact/base/urls.py
+++ djcontact/base/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from . import views
+from .views import homepage, successView
+from django.contrib import admin
+from django.urls import path
 
 app_name =  "main"
 
+from .views import homepage, successView
+
 urlpatterns = [
     path('', views.homepage, name='base-home'),
+    path('email/', views.homepage, name='email'),
+    path('success/', successView, name='success'),
 ]
