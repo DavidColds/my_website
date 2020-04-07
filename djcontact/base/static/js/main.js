bd68efb0f7fe177efbab9875813101dd7d3117a0
David Drughi <drughidavid6@gmail.com>
2020-04-07 10:02:06 +0200

    fixed small bugs in navbar

--- djcontact/base/static/js/main.js
+++ djcontact/base/static/js/main.js
@@ -1,98 +1,72 @@
-var dummyContent = $('.dummy-content').children(),
-    i;
+var $content = $('header .content'),
+  $blur = $('header .overlay'),
+  wHeight = $(window).height();
 
-
-$('#add-content').click(function (e) {
-    e.preventDefault();
-
-    if ($(dummyContent[0]).is(":visible")) {
-        for (i = 0; i < dummyContent.length; i++) {
-            $(dummyContent[i]).fadeOut(600);
-        }
-    } else {
-        for (i = 0; i < dummyContent.length; i++) {
-            $(dummyContent[i]).delay(600 * i).fadeIn(600);
-        }
-    }
-
+$(window).on('resize', function () {
+  wHeight = $(window).height();
 });
 
-
-
-var $content = $('header .content')
-, $blur    = $('header .overlay')
-, wHeight  = $(window).height();
-
-$(window).on('resize', function(){
-wHeight = $(window).height();
-});
-
-window.requestAnimFrame = (function()
-{
-return  window.requestAnimationFrame       ||
-        window.webkitRequestAnimationFrame ||
-        window.mozRequestAnimationFrame    ||
-        function( callback ){
-          window.setTimeout(callback, 1000 / 60);
-        };
+window.requestAnimFrame = (function () {
+  return window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    function (callback) {
+      window.setTimeout(callback, 1000 / 60);
+    };
 })();
 
-function Scroller()
-{
-this.latestKnownScrollY = 0;
-this.ticking            = false;
+function Scroller() {
+  this.latestKnownScrollY = 0;
+  this.ticking = false;
 }
 
 Scroller.prototype = {
 
-init: function() {
-  window.addEventListener('scroll', this.onScroll.bind(this), false);
-  $blur.css('background-image',$('header:first-of-type').css('background-image'));
-},
+  init: function () {
+    window.addEventListener('scroll', this.onScroll.bind(this), false);
+    $blur.css('background-image', $('header:first-of-type').css('background-image'));
+  },
 
 
-onScroll: function() {
-  this.latestKnownScrollY = window.scrollY;
-  this.requestTick();
-},
+  onScroll: function () {
+    this.latestKnownScrollY = window.scrollY;
+    this.requestTick();
+  },
 
 
-requestTick: function() {
-  if( !this.ticking ) {
-    window.requestAnimFrame(this.update.bind(this));
+  requestTick: function () {
+    if (!this.ticking) {
+      window.requestAnimFrame(this.update.bind(this));
+    }
+    this.ticking = true;
+  },
+
+  update: function () {
+    var currentScrollY = this.latestKnownScrollY;
+    this.ticking = false;
+
+
+    var slowScroll = currentScrollY / 2,
+      blurScroll = currentScrollY * 2,
+      opaScroll = 1.4 - currentScrollY / 400;
+    if (currentScrollY > wHeight)
+      $('nav').css('position', 'fixed');
+    else
+      $('nav').css('position', 'absolute');
+
+    $content.css({
+      'transform': 'translateY(' + slowScroll + 'px)',
+      '-moz-transform': 'translateY(' + slowScroll + 'px)',
+      '-webkit-transform': 'translateY(' + slowScroll + 'px)',
+      'opacity': opaScroll
+    });
+
+    $blur.css({
+      'opacity': blurScroll / wHeight
+    });
   }
-  this.ticking = true;
-},
-
-update: function() {
-  var currentScrollY = this.latestKnownScrollY;
-  this.ticking       = false;
-  
-  
-  var slowScroll = currentScrollY / 2
-    , blurScroll = currentScrollY * 2
-    , opaScroll = 1.4 - currentScrollY / 400;
- if(currentScrollY > wHeight)
-   $('nav').css('position','fixed');
- else
-   $('nav').css('position','absolute');
-  
-  $content.css({
-    'transform'         : 'translateY(' + slowScroll + 'px)',
-    '-moz-transform'    : 'translateY(' + slowScroll + 'px)',
-    '-webkit-transform' : 'translateY(' + slowScroll + 'px)',
-    'opacity' : opaScroll
-  });
-  
-  $blur.css({
-    'opacity' : blurScroll / wHeight
-  });
-}
 };
 
 
-var scroller = new Scroller();  
+var scroller = new Scroller();
 scroller.init();
-
-
-  
