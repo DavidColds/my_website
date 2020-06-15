6f863f66a96d686446243487f4fd863b943c046c
David Drughi <drughidavid6@gmail.com>
2020-06-15 07:33:14 +0200

    added Testimonials

--- static/js/main.js
+++ static/js/main.js
@@ -1,36 +1,91 @@
 // Sticky Header
-$(window).scroll(function() {
+$(window).scroll(function () {
 
-    if ($(window).scrollTop() > 100) {
-        $('.main_h').addClass('sticky');
-    } else {
-        $('.main_h').removeClass('sticky');
-    }
+  if ($(window).scrollTop() > 100) {
+    $('.main_h').addClass('sticky');
+  } else {
+    $('.main_h').removeClass('sticky');
+  }
 });
 
 // Mobile Navigation
-$('.mobile-toggle').click(function() {
-    if ($('.main_h').hasClass('open-nav')) {
-        $('.main_h').removeClass('open-nav');
-    } else {
-        $('.main_h').addClass('open-nav');
-    }
+$('.mobile-toggle').click(function () {
+  if ($('.main_h').hasClass('open-nav')) {
+    $('.main_h').removeClass('open-nav');
+  } else {
+    $('.main_h').addClass('open-nav');
+  }
 });
 
-$('.main_h li a').click(function() {
-    if ($('.main_h').hasClass('open-nav')) {
-        $('.navigation').removeClass('open-nav');
-        $('.main_h').removeClass('open-nav');
-    }
+$('.main_h li a').click(function () {
+  if ($('.main_h').hasClass('open-nav')) {
+    $('.navigation').removeClass('open-nav');
+    $('.main_h').removeClass('open-nav');
+  }
 });
 
 // navigation scroll lijepo radi materem
-$('nav a').click(function(event) {
-    var id = $(this).attr("href");
-    var offset = 70;
-    var target = $(id).offset().top - offset;
-    $('html, body').animate({
-        scrollTop: target
-    }, 500);
-    event.preventDefault();
-}); 
+$('nav a').click(function (event) {
+  var id = $(this).attr("href");
+  var offset = 70;
+  var target = $(id).offset().top - offset;
+  $('html, body').animate({
+    scrollTop: target
+  }, 500);
+  event.preventDefault();
+});
+
+
+$(function () {
+  var selectedClass = "";
+  $(".filter").click(function () {
+    selectedClass = $(this).attr("data-rel");
+    $("#gallery").fadeTo(100, 0.1);
+    $("#gallery div").not("." + selectedClass).fadeOut().removeClass('animation');
+    setTimeout(function () {
+      $("." + selectedClass).fadeIn().addClass('animation');
+      $("#gallery").fadeTo(300, 1);
+    }, 300);
+  });
+});
+
+
+$(function () {
+  var selectedClass = "";
+  $(".filter").click(function () {
+    selectedClass = $(this).attr("data-rel");
+    $("#gallery").fadeTo(100, 0.1);
+    $("#gallery div").not("." + selectedClass).fadeOut().removeClass('animation');
+    setTimeout(function () {
+      $("." + selectedClass).fadeIn().addClass('animation');
+      $("#gallery").fadeTo(300, 1);
+    }, 300);
+  });
+});
+
+
+jQuery(document).ready(function ($) {
+  "use strict";
+  //  TESTIMONIALS CAROUSEL HOOK
+  $('#customers-testimonials').owlCarousel({
+    loop: true,
+    center: true,
+    items: 3,
+    margin: 0,
+    autoplay: true,
+    dots: true,
+    autoplayTimeout: 8500,
+    smartSpeed: 350,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 2
+      },
+      1170: {
+        items: 3
+      }
+    }
+  });
+});
