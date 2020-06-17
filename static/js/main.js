a4a433b68f39a15c24df803b22bd3237583423b6
David Drughi <drughidavid6@gmail.com>
2020-06-17 13:51:41 +0200

    remade the landing page

--- static/js/main.js
+++ static/js/main.js
@@ -89,3 +89,133 @@ jQuery(document).ready(function ($) {
     }
   });
 });
+
+
+// VARIABLES
+const magicalUnderlines = Array.from(document.querySelectorAll('.underline--magical'));
+
+const gradientAPI = 'https://gist.githubusercontent.com/wking-io/3e116c0e5675c8bcad8b5a6dc6ca5344/raw/4e783ce3ad0bcd98811c6531e40256b8feeb8fc8/gradient.json';
+
+// HELPER FUNCTIONS
+
+// 1. Get random number in range. Used to get random index from array.
+const randNumInRange = max => Math.floor(Math.random() * (max - 1));
+
+// 2. Merge two separate array values at the same index to 
+// be the same value in new array.
+const mergeArrays = (arrOne, arrTwo) => arrOne.
+map((item, i) => `${item} ${arrTwo[i]}`).
+join(', ');
+
+// 3. Curried function to add a background to array of elms
+const addBackground = elms => color => {
+  elms.forEach(el => {
+    el.style.backgroundImage = color;
+  });
+};
+// 4. Function to get data from API
+const getData = async url => {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data.data;
+};
+
+// 5. Partial Application of addBackground to always apply 
+// background to the magicalUnderlines constant
+const addBackgroundToUnderlines = addBackground(magicalUnderlines);
+
+// GRADIENT FUNCTIONS
+
+// 1. Build CSS formatted linear-gradient from API data
+const buildGradient = obj => `linear-gradient(${obj.direction}, ${mergeArrays(obj.colors, obj.positions)})`;
+
+// 2. Get single gradient from data pulled in array and
+// apply single gradient to a callback function
+const applyGradient = async (url, callback) => {
+  const data = await getData(url);
+  const gradient = buildGradient(data[randNumInRange(data.length)]);
+  callback(gradient);
+};
+
+// RESULT
+applyGradient(gradientAPI, addBackgroundToUnderlines);
+
+
+
+
+
+
+var ml12 = {};
+ml12.opacityIn = [0,1];
+ml12.scaleIn = [0.2, 1];
+ml12.scaleOut = 3;
+ml12.durationIn = 800;
+ml12.durationOut = 600;
+ml12.delay = 500;
+
+anime.timeline({loop: true})
+.add({
+  targets: '.ml12 .letters-1',
+  translateX: [40,0],
+  translateZ: 0,
+  opacity: [0,1],
+  easing: "easeOutExpo",
+  duration: 1200,
+  delay: (el, i) => 500 + 30 * i
+}).add({
+  targets: '.ml12 .letters-1',
+  translateX: [0,-30],
+  opacity: [1,0],
+  easing: "easeInExpo",
+  duration: 1100,
+  delay: (el, i) => 100 + 30 * i
+})
+.add({
+  targets: '.ml12 .letters-2',
+  translateX: [40,0],
+  translateZ: 0,
+  opacity: [0,1],
+  easing: "easeOutExpo",
+  duration: 1200,
+  delay: (el, i) => 500 + 30 * i
+}).add({
+  targets: '.ml12 .letters-2',
+  translateX: [0,-30],
+  opacity: [1,0],
+  easing: "easeInExpo",
+  duration: 1100,
+  delay: (el, i) => 100 + 30 * i
+})
+.add({
+  targets: '.ml12 .letters-3',
+  translateX: [40,0],
+  translateZ: 0,
+  opacity: [0,1],
+  easing: "easeOutExpo",
+  duration: 1200,
+  delay: (el, i) => 500 + 30 * i
+}).add({
+  targets: '.ml12 .letters-3',
+  translateX: [0,-30],
+  opacity: [1,0],
+  easing: "easeInExpo",
+  duration: 1100,
+  delay: (el, i) => 100 + 30 * i
+})
+.add({
+  targets: '.ml12 .letters-4',
+  translateX: [40,0],
+  translateZ: 0,
+  opacity: [0,1],
+  easing: "easeOutExpo",
+  duration: 1200,
+  delay: (el, i) => 500 + 30 * i
+}).add({
+  targets: '.ml12 .letters-4',
+  translateX: [0,-30],
+  opacity: [1,0],
+  easing: "easeInExpo",
+  duration: 1200,
+  delay: (el, i) => 100 + 30 * i
+})
+;
